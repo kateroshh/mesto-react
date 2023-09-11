d3f22a757a87ddadf9f506893b22985bbde38df7
Kate Roshchupkina <katerina.ermakov@mail.ru>
2023-09-11 09:57:02 +0300

    sprint 12

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,41 @@
-import logo from '../images/header__logo.svg';
+import logo from "../images/header__logo.svg";
+import menu from "../images/menu.svg";
+import closeIcon from "../images/close_mobile.svg";
+import { useState } from "react";
 
 function Header() {
-    return (
-        <header className="header">
-            <img className="header__logo" src={logo} alt="Логотип" />
-        </header>
-    );
+  const [isMenu, setIsMenu] = useState(true);
+
+  function handleClickMenu() {
+    document
+      .querySelector(".header__text")
+      .classList.toggle("header__menu_active");
+
+    setIsMenu(!isMenu);
   }
-  
-  export default Header;
+
+  return (
+    <>
+      <header className='header'>
+        <p className='header__text'>
+          email@mail.com
+          <a className='header__exit' href='#'>
+            Выйти
+          </a>
+        </p>
+        <div className='header__main'>
+          <img className='header__logo' src={logo} alt='Логотип' />
+
+          <img
+            className='header__menu'
+            src={isMenu ? menu : closeIcon}
+            alt='Меню'
+            onClick={handleClickMenu}
+          />
+        </div>
+      </header>
+    </>
+  );
+}
+
+export default Header;
